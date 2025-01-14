2e4b3357e1aee3bc28fb5f6d07bf18945ce5b8b9
Kevindecastro <9830@holbertonstudents.com>
2025-01-14 14:16:25 +0100

    correction of code by chat gpt

---
+++ debugging/print_arguments.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_arguments():
+	"""
+	Print all arguments passed to the script except the script name.
+
+	Parameters:
+		None
+
+	Returns:
+		None
+	"""
+	for arg in sys.argv[1:]:
+		print(arg)
+
+
+if __name__ == "__main__":
+	print_arguments()
