eda79ffa179990f7de02e61be7f3174c635b8434
Kevindecastro <9830@holbertonstudents.com>
2025-01-16 15:16:00 +0100

    correction of code

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -3,18 +3,18 @@ import sys
 
 
 def print_arguments():
-	"""
-	Print all arguments passed to the script except the script name.
+    """
+    Print all arguments passed to the script except the script name.
 
-	Parameters:
-		None
+    Parameters:
+        None
 
-	Returns:
-		None
-	"""
-	for arg in sys.argv[1:]:
-		print(arg)
+    Returns:
+        None
+    """
+    for arg in sys.argv[1:]:
+        print(arg)
 
 
 if __name__ == "__main__":
-	print_arguments()
+    print_arguments()
