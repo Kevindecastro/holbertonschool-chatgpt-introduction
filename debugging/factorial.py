c67200062c2c788afac02d481dc8cec1b79cd020
Kevindecastro <9830@holbertonstudents.com>
2025-01-16 15:13:57 +0100

    correction of code

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
-	"""Calculates the factorial of a given number."""
-	result = 1
-	while n > 1:
-		result *= n
-		n -= 1
-	return result
+    """Calculates the factorial of a given number."""
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Usage: ./factorial.py <number>")
-		sys.exit(1)
-	else:
-		try:
-			n = int(sys.argv[1])
-			print(factorial(n))
-		except ValueError:
-			print("Please provide a valid integer.")
-			sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <number>")
+        sys.exit(1)
+    else:
+        try:
+            n = int(sys.argv[1])
+            print(factorial(n))
+        except ValueError:
+            print("Please provide a valid integer.")
+            sys.exit(1)
